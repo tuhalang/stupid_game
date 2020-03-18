a447931379bc30c516363e56735da10a898c6999
tuhalang <tuhalang007@outlook.com.vn>
2020-03-18 21:02:38 +0700

    add model test

---
+++ model/src/org/example/model/Plane.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.example.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tuhalang
+ */
+public class Plane extends Point{
+    
+    private int numberOfBullet;
+    private ArrayList<Bullet> bullets;
+
+    @Override
+    public void move() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ model/src/org/example/model/Player.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.example.model;
+
+/**
+ *
+ * @author tuhalang
+ */
+public class Player {
+    private String username;
+    private String token;
+    private int score;
+    private Plane plane;
+}
